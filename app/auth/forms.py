a07e52b4f8e0a9edaf737fe966410b3692777a19
Chirag Parmar <chiragkparmar@gmail.com>
2017-03-09 22:36:09 -0500

    Update login class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,23 +5,36 @@ from wtforms import ValidationError
 from ..models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
-                                             Email()])
+    email_or_username = StringField(
+        'Email or Username', validators=[Required(), Length(1, 64)]
+    )
     password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
-                                           Email()])
-    username = StringField('Username', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                          'Usernames must have only letters, '
-                                          'numbers, dots or underscores')])
-    password = PasswordField('Password', validators=[
-        Required(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[Required()])
+    email = StringField(
+        'Email', validators=[Required(), Length(1, 64), Email()]
+    )
+    username = StringField(
+        'Username',
+        validators=[
+            Required(), Length(1, 64),
+            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                   'Usernames must have only letters, '
+                   'numbers, dots or underscores')
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            Required(),
+            EqualTo('password_confirmation', message='Passwords must match.')
+        ]
+    )
+    password_confirmation = PasswordField(
+        'Confirm password', validators=[Required()]
+    )
     submit = SubmitField('Register')
 
     def validate_email(self, field):
