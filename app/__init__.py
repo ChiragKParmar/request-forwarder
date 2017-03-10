a97606515ba5cf87d432520fec52930b474b23e2
Chirag Parmar <chiragkparmar@gmail.com>
2017-03-09 22:35:10 -0500

    Add login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_wtf import FlaskForm, CSRFProtect
 from flask_bootstrap import Bootstrap
 from config import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -16,6 +20,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     csrf.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
