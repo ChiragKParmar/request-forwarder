7ccdc837d7378cd19050206285701a042a68a2e2
Chirag Parmar <chiragkparmar@gmail.com>
2017-03-09 22:37:28 -0500

    Update data models

--- app/models/proxyurl.py
+++ app/models/proxyurl.py
@@ -1,5 +1,141 @@
+import re
+from datetime import datetime
+
+from sqlalchemy.orm import synonym
 from werkzeug.security import generate_password_hash, check_password_hash
-from .. import db
+from flask import url_for
+from flask_login import UserMixin
+
+from app import db, login_manager
+
+EMAIL_REGEX = re.compile(r'^\S+@\S+\.\S+$')
+USERNAME_REGEX = re.compile(r'^\S+$')
+
+
+def check_length(attribute, length):
+    """Checks the attribute's length."""
+    try:
+        return bool(attribute) and len(attribute) <= length
+    except:
+        return False
+
+
+class BaseModel:
+    """Base for all models, providing save, delete and from_dict methods."""
+
+    def __commit(self):
+        """Commits the current db.session, does rollback on failure."""
+        from sqlalchemy.exc import IntegrityError
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+    def delete(self):
+        """Deletes this model from the db (through db.session)"""
+        db.session.delete(self)
+        self.__commit()
+
+    def save(self):
+        """Adds this model to the db (through db.session)"""
+        db.session.add(self)
+        self.__commit()
+        return self
+
+    @classmethod
+    def from_dict(cls, model_dict):
+        return cls(**model_dict).save()
+
+
+class User(UserMixin, db.Model, BaseModel):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    _username = db.Column('username', db.String(64), unique=True)
+    _email = db.Column('email', db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+    member_since = db.Column(db.DateTime, default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    # todolists = db.relationship('TodoList', backref='user', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+    def __repr__(self):
+        if self.is_admin:
+            return '<Admin {0}>'.format(self.username)
+        return '<User {0}>'.format(self.username)
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        is_valid_length = check_length(username, 64)
+        if not is_valid_length or not bool(USERNAME_REGEX.match(username)):
+            raise ValueError('{} is not a valid username'.format(username))
+        self._username = username
+
+    username = synonym('_username', descriptor=username)
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        if not check_length(email, 64) or not bool(EMAIL_REGEX.match(email)):
+            raise ValueError('{} is not a valid email address'.format(email))
+        self._email = email
+
+    email = synonym('_email', descriptor=email)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        if not bool(password):
+            raise ValueError('no password given')
+
+        hashed_password = generate_password_hash(password)
+        if not check_length(hashed_password, 128):
+            raise ValueError('not a valid password, hash is too long')
+        self.password_hash = hashed_password
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def seen(self):
+        self.last_seen = datetime.utcnow()
+        return self.save()
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'user_url': url_for(
+                'api.get_user', username=self.username, _external=True
+            ),
+            'member_since': self.member_since,
+            'last_seen': self.last_seen
+            # ,
+            # 'todolists': url_for(
+            #     'api.get_user_todolists',
+            #     username=self.username, _external=True
+            # ),
+            # 'todolist_count': self.todolists.count()
+        }
+
+    def promote_to_admin(self):
+        self.is_admin = True
+        return self.save()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class ProxyUrl(db.Model):
     __tablename__ = 'proxy'
@@ -9,33 +145,3 @@ class ProxyUrl(db.Model):
 
     def __repr__(self):
         return '<ProxyUrl %r>' % self.incoming_url
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
